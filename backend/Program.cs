49be29af8ff5ecb99648486036449a4d1b4e3e6f
kanishk0808 <kanishk0808@gmail.com>
2026-03-25 02:38:59 +0800

    fearure: add rooms endpoint

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,4 +3,14 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/rooms", () =>
+{
+    var rooms = new[]
+    {
+        new { Id = 1, Name = "Room A", Capacity = 4}, 
+        new { Id = 2, Name = "Room B", Capacity = 6}
+    };
+    return rooms;
+});
+
 app.Run();
