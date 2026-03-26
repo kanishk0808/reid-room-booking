8381b445d4a8a361e6fb06e20d56b57edb9ba7e3
kanishk0808 <kanishk0808@gmail.com>
2026-03-26 18:16:13 +0800

    chore: file changes

---
+++ backend/Models/Room.cs
@@ -0,0 +1,6 @@
+public class Room
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public int Capacity { get; set; }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,16 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var rooms = new List<string>
+{
+    "Room A",
+    "Room B",
+    "Room C"
+};
+
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/rooms", () =>
-{
-    var rooms = new[]
-    {
-        new { Id = 1, Name = "Room A", Capacity = 4}, 
-        new { Id = 2, Name = "Room B", Capacity = 6}
-    };
-    return rooms;
-});
+
+app.MapGet("/rooms", () => rooms);
 
 app.Run();
