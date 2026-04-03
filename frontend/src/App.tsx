1fef3a75c2df93980d959611a8d1a2ab5e98c92b
kanishk0808 <kanishk0808@gmail.com>
2026-04-03 22:13:24 +0800

    feature: installed tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,18 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
